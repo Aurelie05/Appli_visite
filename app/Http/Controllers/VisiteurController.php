265791213a1e7b63ed945cdeef702ecf4a96b456
Aurelie05 <roxaneakredjoro@gmail.com>
2026-02-10 22:00:57 +0000

    OCR2

--- app/Http/Controllers/VisiteurController.php
+++ app/Http/Controllers/VisiteurController.php
@@ -7,6 +7,7 @@ use App\Models\Visiteur;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
 
 class VisiteurController extends Controller
